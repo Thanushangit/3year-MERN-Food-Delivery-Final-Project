56ea23aa09ebda3675f78cc32585b7ab56148ac0
Thanushangit <sinnarasathanushan2002@gmail.com>
2025-05-17 12:33:21 +0530

    customer-navbar-

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import CustomerNavbar from './Componets/CustomerNavbar'
+import './main.css'
+
+function App() {
+  return (
+    <div>
+      <CustomerNavbar/>
+    </div>
+  )
+}
+
+export default App
