6412da710de56c5ce3a47de57c240ad6cd963269
Thanushan <sinnarasathanushan2002@gmail.com>
2025-06-17 20:53:38 +0530

    animation work

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import 'swiper/css/bundle';
 import 'react-toastify/dist/ReactToastify.css';
+import ScrollReveal from "scrollreveal";
 import { ToastContainer } from 'react-toastify';
 import CustomerNavbar from '../Componets/Navbar/CustomerNavbar'
 import HomeFeaturesCard from '../Componets/HomePage/HomeFeaturesCard';
@@ -11,9 +12,32 @@ import HeroSection from '../Componets/HomePage/HeroSection';
 import MobileAppSection from '../Componets/HomePage/MobileAppSection';
 import FoodMenuButtons from '../Componets/FoodMenu/FoodMenuButtons';
 import NewsLetter from '../Componets/HomePage/NewsLetter';
+import { useEffect } from 'react';
 
 
 const Home = () => {
+    useEffect(() => {
+        const sr = ScrollReveal({
+            origin: "top",
+            distance: "60px",
+            duration: 2500,
+            delay: 300,
+            reset: false,
+        });
+
+        sr.reveal(`.hero_text,.cart_heading`);
+        sr.reveal(`.hero_image`, { delay: 500, scale: 0.5 });
+        sr.reveal(`.service_card,.food_item`, { interval: 100, delay: 200 });
+        sr.reveal(`.mobile,.newsletter_bg`);
+        sr.reveal(`.mobile_content`, { origin: "left" });
+        sr.reveal(`.food_item_container`, { origin: "left", delay: 200 });
+        sr.reveal(`.footer`, { origin: "bottom", duration: 2000, delay: 200, mobile: false });
+        sr.reveal(`.gallery_item`, { scale: 0.5, interval: 100 });
+        sr.reveal(`.customer_container`, { duration: 3000 });
+        sr.reveal(`.dishes_item`, { interval: 100, delay: 200 });
+        sr.reveal(`.newsletter_text`, { origin: "bottom", duration: 3500, delay: 500, mobile: false });
+    }, []);
+
     return (
         <div>
 
@@ -57,7 +81,7 @@ const Home = () => {
 
             <NewsLetter />
 
-          
+
 
             {/* footer  */}
             <Footer />
